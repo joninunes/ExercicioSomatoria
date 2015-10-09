5c023fe1d5fde2a2315c0f788a206d804ef92bd7
Joni L. Nunes <joninunes@gmail.com>
2015-10-09 12:31:35 -0300

    MostraIdadeJava
    Aplicação teste em JAVA contem duas classes
    Inicial
    Inicia aplicacao
    Somatoria
    recebe uma idade e mostra mensagem com o uso da JOPtionPane

---
+++ src/com/joninunes/Inicia.java
@@ -0,0 +1,13 @@
+package com.joninunes;
+
+public class Inicia {
+	
+	public static void main(String args[]) {
+		Somatoria idade;
+		
+		idade = new Somatoria();
+		idade.soma();
+		System.exit(0);
+	}
+
+}
---
+++ src/com/joninunes/Somatoria.java
@@ -0,0 +1,13 @@
+package com.joninunes;
+
+import javax.swing.JOptionPane;
+
+public class Somatoria {
+
+	public void soma(){
+		String idade;
+		idade = JOptionPane.showInputDialog("Informe sua idade: ");
+		JOptionPane.showMessageDialog(null, idade);
+	}
+	
+}
